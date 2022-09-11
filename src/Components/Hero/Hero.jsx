883f78631a43080f1be4ea3b69eb7682491127e1
mateofumis <mateofumis1@gmail.com>
2022-09-11 13:18:24 -0300

    the parts of the app

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Hero = () => {
+  return <div></div>;
+};
+export default Hero;
---
+++ src/Components/Movies/Movies.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Movies = () => {
+  return <div></div>;
+};
+export default Movies;
