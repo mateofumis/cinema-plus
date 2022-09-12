f44c787fde51b424e7fc0477467eaf23529621aa
mateofumis <mateofumis1@gmail.com>
2022-09-12 17:09:27 -0300

    first commit

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-const Navigation = () => {
-  return <div></div>;
+import "./Navigation.css";
+const Navigation = (prop) => {
+  return (
+    <nav className="nav">
+      <p>Películas</p>
+      <p>Series</p>
+      <p>Top 10 {prop.month}</p>
+      <p>Mi Lista</p>
+    </nav>
+  );
 };
 export default Navigation;
